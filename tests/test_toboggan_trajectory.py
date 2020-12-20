7e981834fa7089d3526765bd227ba0a286e3088d
Fernando Martínez González <frndmartinezglez@gmail.com>
2020-12-20 14:34:23 +0100

    test: add test for main functions

--- tests/test_toboggan_trajectory.py
+++ tests/test_toboggan_trajectory.py
@@ -1,4 +1,5 @@
 from toboggan_trajectory import count_trees, multiply_trees
+from toboggan_trajectory.__main__ import main, main2
 
 
 def test_count_trees():
@@ -27,3 +28,51 @@ def test_muliply_trees():
 #.##...#...
 #...##....#
 .#..#...#.#""") == 336
+
+
+def test_count_trees_main(capsys, mocker):
+    mocker.patch(
+        'fileinput.input',
+        return_value=[
+            '..##.......\n',
+            '#...#...#..\n',
+            '.#....#..#.\n',
+            '..#.#...#.#\n',
+            '.#...##..#.\n',
+            '..#.##.....\n',
+            '.#.#.#....#\n',
+            '.#........#\n',
+            '#.##...#...\n',
+            '#...##....#\n',
+            '.#..#...#.#',
+        ]
+    )
+
+    main()
+
+    captured = capsys.readouterr()
+    assert captured.out == '7\n'
+
+
+def test_muliply_trees_main(capsys, mocker):
+    mocker.patch(
+        'fileinput.input',
+        return_value=[
+            '..##.......\n',
+            '#...#...#..\n',
+            '.#....#..#.\n',
+            '..#.#...#.#\n',
+            '.#...##..#.\n',
+            '..#.##.....\n',
+            '.#.#.#....#\n',
+            '.#........#\n',
+            '#.##...#...\n',
+            '#...##....#\n',
+            '.#..#...#.#',
+        ]
+    )
+
+    main2()
+
+    captured = capsys.readouterr()
+    assert captured.out == '336\n'
