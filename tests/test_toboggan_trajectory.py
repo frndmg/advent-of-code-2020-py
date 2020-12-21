c1f6afc5ecaeafa3a99698d67bc26c897ec0e071
Fernando Martínez González <frndmartinezglez@gmail.com>
2020-12-21 11:13:07 +0100

    test: remove redundant tests

--- tests/test_toboggan_trajectory.py
+++ tests/test_toboggan_trajectory.py
@@ -1,33 +1,4 @@
-from toboggan_trajectory import count_trees, multiply_trees
 from toboggan_trajectory.__main__ import main, main2
-
-
-def test_count_trees():
-    assert count_trees("""..##.......
-#...#...#..
-.#....#..#.
-..#.#...#.#
-.#...##..#.
-..#.##.....
-.#.#.#....#
-.#........#
-#.##...#...
-#...##....#
-.#..#...#.#""") == 7
-
-
-def test_muliply_trees():
-    assert multiply_trees("""..##.......
-#...#...#..
-.#....#..#.
-..#.#...#.#
-.#...##..#.
-..#.##.....
-.#.#.#....#
-.#........#
-#.##...#...
-#...##....#
-.#..#...#.#""") == 336
 
 
 def test_count_trees_main(capsys, mocker):
